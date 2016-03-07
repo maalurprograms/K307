dee207fe715757e3f6b1dde11811f35fe66a276d
John Francis <jfswitz@gmail.com>
2016-03-07 15:10:12 +0100

    Website Design Beta - Login, Reg & Home

---
+++ website/register.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+  <title>#Grades - Register</title>
+  <link rel="icon" type="image/png" href="static/images/favicon.png">
+  <link rel="stylesheet" type="text/css" href="static/styles.css">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div id="register_container">
+  <img id="icon" src="static/images/register.png" />
+    <h2>Register</h2>
+    <form id="register_form">
+      <label for="username">Userame </label><br>
+      <input id="username" class="form_elements" type="text" name="name" required/><br>
+      <label for="password">Password </label><br>
+      <input id="password" class="form_elements" type="password" name="password" required/><br>
+      <label for="cpassword">Confirm password </label><br>
+      <input id="cpassword" class="form_elements" type="password" name="cpassword" required/><br>
+      <input id="submit" type="submit" value="Submit">
+    </form>
+  </div>
+</body>
+</html>
