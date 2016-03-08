bdf7882f079d8d91f1bb347a72dd40b6c0b243f6
Jonas Cosandey <jonascosandey@hotmail.com>
2016-03-08 14:35:24 +0100

    model class bug fixing

--- lib/Model.php
+++ lib/Model.php
@@ -7,41 +7,49 @@ class Model
     public static function getUser($username)
     {
         $query = "SELECT userID, username, password FROM users WHERE username = ?";
-        return self::sendQuery($query, true, true, array("s", $username));
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param("s", $username);
+        return self::sendQuery($statement, true);
 
     }
 
     public static function addUser($username, $password){
         $query = 'insert into users (username, passwort) VALUES (?, ?)';
-        self::sendQuery($query, false, true, array("ss", array($username, $password)));
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param("ss", $username,$password);
+        self::sendQuery($statement, false);
     }
 
     public static function deleteUser($username)
     {
         $query = "DELETE FROM users WHERE username = ?";
-        self::sendQuery($query, false, true, array("s", $username));
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param("s", $username);
+        self::sendQuery($statement, false);
     }
 
     public static function getAllNotesFromUser($userid){
         $query = "select name, date, content from notes inner join users on users.userID=notes.IDuser where userID = ?";
-        return self::sendQuery($query, true, true, array("i", $userid));
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param("i", $userid);
+        return self::sendQuery($statement, true);
     }
 
     public static function addNote($name, $date, $content, $userid){
         $query = 'insert into notes (name, date, content, IDuser) VALUES (?, ?, ?, ?)';
-        self::sendQuery($query, false, true, array("sssi", array($name, $date, $content, $userid)));
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param("sssi", $name,$date, $content, $userid);
+        self::sendQuery($statement, false);
     }
 
     public static function deleteNote($id){
         $query = "DELETE FROM notes WHERE noteID = ?";
-        self::sendQuery($query, false, true, array("i",$id));
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param("i", $id);
+        self::sendQuery($statement, false);
     }
 
-    private static function sendQuery($query, $needOutput, $bindParam, $paramlist=null){
-        $statement = ConnectionHandler::getConnection()->prepare($query);
-        if($bindParam){
-            $statement->bind_param($paramlist[0], $paramlist[1]);
-        }
+    private static function sendQuery($statement, $needOutput){
         $statement->execute();
         $result = $statement->get_result();
         if (!$result) {
