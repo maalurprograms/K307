a85b35e14a0685eeed7f253c9cc71f128998e9a7
Jonas Cosandey <jonascosandey@hotmail.com>
2016-03-08 09:06:05 +0100

    made mvc structure

--- controller/login.php
+++ controller/login.php
@@ -1,13 +1,13 @@
 <html>
 <head>
   <title>#Grades - Login</title>
-  <link rel="icon" type="image/png" href="static/images/favicon.png">
-  <link rel="stylesheet" type="text/css" href="static/styles.css">
+  <link rel="icon" type="image/png" href="../view/static/images/favicon.png">
+  <link rel="stylesheet" type="text/css" href="../view/static/styles.css">
   <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 </head>
 <body>
   <div id="form_container">
-  <img id="icon" src="static/images/icon.png" />
+  <img id="icon" src="../view/static/images/icon.png" />
     <h2>Log In</h2>
     <form id="login_form">
       <label for="username">Username </label><br>
