ebe2dda7746fb0e58fe6e4aa4b077f2081ea47eb
John Francis <jfswitz@gmail.com>
2016-03-08 08:34:00 +0100

    gui

--- website/controller.php
+++ website/controller.php
@@ -1,10 +1,4 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: bmahaj
- * Date: 07.03.2016
- * Time: 15:21
- *
- *
  * Verbinder zwischen DB und view.php
  */
--- website/view.php
+++ website/view.php
@@ -1,9 +1,4 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: bmahaj
- * Date: 07.03.2016
- * Time: 15:21
- *
  * Verbinder zwischen client side und controller.php
  */
