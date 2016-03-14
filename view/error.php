3702e84faa8b899c40a4745edbc9497c30efd707
Jonas Cosandey <jonascosandey@hotmail.com>
2016-03-14 14:55:03 +0100

    ugly button bug fixed it is ~ Yoda

--- view/error.php
+++ view/error.php
@@ -16,8 +16,8 @@
         <div class="col-md-1"></div>
         <div class="col-md-7"><h1><b>SwissNotes</b></h1></div>
         <div class="col-md-3 padding20">
-            <a href="default/login"><button id="login">Log in</button></a>
-            <a href="default/register"><button id="login">Register</button></a>
+            <a href="default/login"><button class="login">Log in</button></a>
+            <a href="default/register"><button class="login">Register</button></a>
         </div>
         <div class="col-md-1"></div>
     </div>
